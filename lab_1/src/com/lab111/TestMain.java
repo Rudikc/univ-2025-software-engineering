a2d9444cdc7639df6a07ce395e0510cda8df9ab5
Maksym Rudichenko <maksymr@lyft.com>
2025-09-27 23:33:42 +0300

    Fix

---
+++ lab_1/src/com/lab111/TestMain.java
@@ -0,0 +1,7 @@
+package com.lab111;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("TestMain is running. Project setup is ready.");
+    }
+}
--- lab_1/src/com/main/TestMain.java
+++
@@ -1,7 +0,0 @@
-package com.lab1;
-
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("Hello, World from lab 1 built with Ant!");
-    }
-}
