974022a9a83e674bc5739a7fd616054f7bb12d03
Maksym Rudichenko <maksymr@lyft.com>
2025-09-27 23:07:08 +0300

    Add lab 1 results

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# univ-2025-software-engineering-
 # univ-2025-software-engineering
---
+++ lab_1/src/com/main/TestMain.java
@@ -0,0 +1,7 @@
+package com.lab111;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from lab 1 built with Ant!");
+    }
+}
